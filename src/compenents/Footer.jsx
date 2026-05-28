8e45bce547ee48f68aff4dc09a89b78d5a530f77
EduardoMoraess <eduzinho2118@gmail.com>
2026-05-28 18:32:00 -0300

    akshdj

--- src/compenents/Footer.jsx
+++ src/compenents/Footer.jsx
@@ -3,6 +3,10 @@ function Footer() {
         <footer className="main-footer">
             <p>"Onde estiverem dois ou três reunidos em meu nome, ali estou."</p>
             <span>Mateus 18:20</span>
+
+            <div className="developer">
+                <h2 className="namedev">Site desenvolvido por Eduardo Moraes</h2>
+            </div>
         </footer>
     )
 }
