ac5a032308a77f24034ef20b166388ee2a4bf90d
EduardoMoraess <eduzinho2118@gmail.com>
2026-07-13 17:39:03 -0300

    agenda de julho

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,34 @@ import InfoSection from './components/InfoSection';
 function App() {
   // Dados da Agenda
   const agendaData = [
-    { dataDay: "terca", hora: "19:30h", dia: "Terça-feira", titulo: "Estudo: Mis. Jailda", diretor: "Francisca" },
-    { dataDay: "quarta", hora: "15:30h", dia: "Quarta-feira", titulo: "🙏 Culto de Oração", diretor: "Dalvinha " },
+    { dataDay: "terca", hora: "19:30h", dia: "Terça-feira", titulo: "Estudo: Mis. Jailda", diretor: "Zenaide" },
+    { dataDay: "quarta", hora: "15:30h", dia: "Quarta-feira", titulo: "🙏 Culto de Oração", diretor: "Mis. Jailda" },
     { dataDay: "quinta", hora: "19:00h", dia: "Quinta-feira", titulo: "Reunião com os ministerios Masc. e Fem." },
     { dataDay: "sexta", hora: "19:30h", dia: "Sexta-feira", titulo: "🔥 Culto de Oração", diretor: "Dc° Márcio", louvor: "Zenaide" },
     { dataDay: "sabado", hora: "19:30h", dia: "Sábado", titulo: "Culto Jovem", matutino: "6:00h - Matutino" },
     {
       dataDay: "domingo", hora: "9:00h", dia: "Domingo - Manhã", titulo: "EBD", featured: true,
       extras: [
-        { label: "Dir", valor: " Alex" },
-        { label: "Estudo/Templo", valor: "Mis. Jailda" },
-        { label: "Discipulado", valor: "Dc. Márcio " }
+        { label: "Dir", valor: "Dalvinha" },
+        { label: "Estudo/Templo", valor: "Junior" },
+        { label: "Discipulado", valor: "Mis. Jailda " }
       ]
     },
     {
       dataDay: "domingo-noite", hora: "19:00h", dia: "Domingo - Noite", titulo: "Culto de Celebração", featured: true,
       extras: [
-        { label: "Dir", valor: "Ministério Jovem" },
-        { label: "Mensagem", valor: "Obreiro Jorge" }
+        { label: "Dir", valor: "Ministério Feminino" },
+        { label: "Mensagem", valor: "Mis. Jailda" }
       ]
     },
   ];
 
   const aniversariantes = [
     // Dias dos aniversarios das pessoas da igreja
-    { destaque: "01", texto: "Yasmin e Mariana" },
-    { destaque: "14", texto: "Obreiro Jorge" },
+    { destaque: "03", texto: "Vanusia" },
+    { destaque: "12", texto: "Dalvinha" },
+    { destaque: "17", texto: "Isabelle" },
+    { destaque: "24", texto: "Vitoria" },
   ];
 
   const pedidosOracao = [
@@ -54,7 +56,7 @@ function App() {
         </div>
         <h1>IBM do Ipsep</h1>
         <p className="subtitle">Agenda Semanal</p>
-        <div className="date-badge">06/07 a 12/07</div>
+        <div className="date-badge">13/07 a 19/07</div>
       </header>
 
       <main className="container">
@@ -65,11 +67,11 @@ function App() {
         </div>
       </main>
 
-      {/*<InfoSection
-        titulo="Aniversariantes do Mês de Junho"
+      <InfoSection
+        titulo="Aniversariantes do Mês de Julho"
         icone="🎂"
         itens={aniversariantes}
-      />*/}
+      />
 
       <InfoSection
         titulo="Pedidos de Oração"
