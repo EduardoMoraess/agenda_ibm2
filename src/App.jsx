0e96a31e215ec5ce9658849f07dd412efec72fb4
EduardoMoraess <eduzinho2118@gmail.com>
2026-07-27 17:04:22 -0300

    atualizada ate agosto

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,24 @@ import InfoSection from './components/InfoSection';
 function App() {
   // Dados da Agenda
   const agendaData = [
-    { dataDay: "terca", hora: "19:30h", dia: "Terça-feira", titulo: "Estudo: Mis. Jailda", diretor: "Maria Costa" },
+    { dataDay: "terca", hora: "19:30h", dia: "Terça-feira", titulo: "Estudo: Mis. Jailda", diretor: "Francisca" },
     { dataDay: "quarta", hora: "15:30h", dia: "Quarta-feira", titulo: "🙏 Culto de Oração", diretor: "Dalvinha" },
-    { dataDay: "quinta", hora: "19:00h", dia: "Quinta-feira", titulo: "Reunião com os ministerios Masc. e Fem." },
-    { dataDay: "sexta", hora: "19:30h", dia: "Sexta-feira", titulo: "🔥 Culto de Oração", diretor: "Isable", louvor: "Zenaide" },
-    { dataDay: "sabado", hora: "19:30h", dia: "Sábado", titulo: "Escola Biblica de Ferias", matutino: "6:00h - Matutino" },
+    { dataDay: "quinta", hora: "19:00h", dia: "Quinta-feira", titulo: "Culto evangelistico na casa de passagem" },
+    { dataDay: "sexta", hora: "19:30h", dia: "Sexta-feira", titulo: "🔥 Culto de Oração", diretor: "Mis. Jailda", louvor: "Zenaide" },
+    { dataDay: "sabado", hora: "19:30h", dia: "Sábado", titulo: "Culto Jovem", matutino: "6:00h - Matutino" },
     {
-      dataDay: "domingo", hora: "9:00h", dia: "Domingo - Manhã", titulo: "EBD", featured: true,
-      extras: [
+      dataDay: "domingo", hora: "9:00h", dia: "Domingo - Manhã", titulo: "Culto de Ceia na sede", featured: true,
+      /*extras: [
         { label: "Dir", valor: "Zenaide" },
         { label: "Estudo/Templo", valor: "Junior" },
         { label: "Discipulado", valor: "Mis. Jailda " }
-      ]
+      ]*/
     },
     {
       dataDay: "domingo-noite", hora: "19:00h", dia: "Domingo - Noite", titulo: "Culto de Celebração", featured: true,
       extras: [
-        { label: "Dir", valor: "Ministério/Evang." },
-        { label: "Mensagem", valor: "Mis. Jailda" }
+        { label: "Dir", valor: "Ministério Masculino" },
+        { label: "Mensagem", valor: "Obreiro Jorge" }
       ]
     },
   ];
@@ -56,7 +56,7 @@ function App() {
         </div>
         <h1>IBM do Ipsep</h1>
         <p className="subtitle">Agenda Semanal</p>
-        <div className="date-badge">20/07 a 26/07</div>
+        <div className="date-badge">27/07 a 02/08</div>
       </header>
 
       <main className="container">
